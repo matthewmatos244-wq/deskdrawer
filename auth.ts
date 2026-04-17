701db4064c85141ad197002a140fdb1d74f28d2c
matthewmatos244-wq <matthewmatos244@gmail.com>
2026-04-17 15:23:53 -0400

    Create auth.ts

---
+++ auth.ts
@@ -0,0 +1,36 @@
+import NextAuth from 'next-auth'
+import Google from 'next-auth/providers/google'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/lib/prisma'
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID!,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET!,
+      authorization: {
+        params: {
+          scope: [
+            'openid',
+            'email',
+            'profile',
+            'https://www.googleapis.com/auth/gmail.readonly',
+            'https://www.googleapis.com/auth/drive.readonly',
+          ].join(' '),
+          access_type: 'offline',
+          prompt: 'consent',
+        },
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, user }) {
+      session.user.id = user.id
+      return session
+    },
+  },
+  pages: {
+    signIn: '/login',
+  },
+})
